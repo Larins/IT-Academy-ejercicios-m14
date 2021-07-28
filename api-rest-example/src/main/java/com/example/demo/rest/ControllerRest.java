421fe83c5214684913f0a802c7c687e26f90ece0
Larins <69240139+Larins@users.noreply.github.com>
2021-07-28 12:59:44 +0200

    update

--- api-rest-example/src/main/java/com/example/demo/rest/ControllerRest.java
+++ api-rest-example/src/main/java/com/example/demo/rest/ControllerRest.java
@@ -1,26 +1,38 @@
 package com.example.demo.rest;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.models.Producto;
 
-@RestController //Indica que esta clase va a ser un servicio REST
-@RequestMapping("/") //Indica en qué URL se van a exponer los servicios de esta clase
+@RestController //Esta clase va a ser un servicio REST
+@RequestMapping("/productos") //En esta URL se van a exponer los servicios de esta clase
 public class ControllerRest {
-	
+
 	@GetMapping
 	public ResponseEntity<Producto> getProducto() {
 		Producto producto = new Producto();
 		producto.setId(1);
-		producto.setName("Producto 1");
+		producto.setName("Producto 1 hola");
 		return ResponseEntity.ok(producto);
 	}
-	
-	//@GetMapping("hello") //Servicio disponible mediante GET (localhost: 8080/)
-	//@RequestMapping(value="hello", method=RequestMethod.GET)
+
+	//@GetMapping("hello") //Servicio disponible mediante GET
+	//RequestMapping(value="hello", method=RequestMethod.GET) //URL en qué está el servicio
 	public String hello() {
-		return "Hello World!";
+		return "hello world";
 	}
+
+	//NOTA SOBRE LAS URL
+	//Podemos especificar la URL que deseemos
+	//La vamos construyendo mediante @RequestMapping y @GetMapping
+	//Ejemplos:
+		//@RequestMapping("/") + @GetMapping("") apunta a localhost:8080/
+		//@RequestMapping("/") + @GetMapping("hello") apunta a localhost:8080/hello
+		//@RequestMapping("/productos") + @GetMapping("cualquiercosa") apunta a localhost:8080/productos/cualquiercosa
+	//Todo lo demás (lo de dentro del public String blabla) queda igual
+
 
 }
